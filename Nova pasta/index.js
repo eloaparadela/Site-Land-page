5b7590a801a8cce121bc8fe304ba52f80ab05a11
eloa paradela <eloaparadela@hotmail.com>
2018-11-06 15:43:11 -0300

    add flex slider

--- Nova pasta/index.js
+++ Nova pasta/index.js
@@ -18,3 +18,15 @@ function aparecerBarra(){
 function none(){
     barra.style.display = "none";
 }
+
+
+$(window).load(function() {
+    $('.flexslider').flexslider({
+      animation: "slide",
+      animationLoop: false,
+      itemWidth: 210,
+      itemMargin: 5,
+      minItems: 2,
+      maxItems: 4
+    });
+  });

528398c86b177368b4cafe92791c1941a0c73d3a
eloa paradela <eloaparadela@hotmail.com>
2018-11-09 15:53:07 -0300

    Ajustando formulario

--- Nova pasta/index.js
+++ Nova pasta/index.js
@@ -19,6 +19,28 @@ function none(){
     barra.style.display = "none";
 }
 
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+
+    var forms = document.getElementsByClassName('needs-validation');
+
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false)
+    });
+  }, false);
+})();
+
+
+// $('.dinheiro').mask('#.##0,00', {reverse: true});
+
+
 
 
 
